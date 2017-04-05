6f4971059b29798d7dd3ff731a87fad8c7073dc2
= <=>
2017-04-06 00:09:24 +0200

    Added JUnit dependency resolution

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,7 +1,16 @@
 package test.java;
 
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import main.java.StringCalculator;
+
 /**
  * Created by robin on 05/04/2017.
  */
-public class StringCalculatorTest {
+public class StringCalculatorTest extends TestCase {
+    public void testSimpleAdd() {
+        StringCalculator stringCalculator = new StringCalculator();
+        int actual = stringCalculator.add("1");
+        Assert.assertEquals(1,actual);
+    }
 }
